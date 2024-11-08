fef46c29d4421d7ed96c021740a648d6df695bf1
Danny Ochoa <danny@dannyochoa.net>
2024-11-07 22:56:46 -0500

    Rename Sections to SectionsHeader

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,4 +1,4 @@
-import Sections from "../ui/sections"
+import SectionsHeader from "../ui/sections-header"
 import Socials from "./socials";
 
 export default function ContactPage() {
@@ -6,7 +6,7 @@ export default function ContactPage() {
         <div className="app">
             <meta charSet="utf-8"></meta>
             <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta> 
-            <Sections/>
+            <SectionsHeader/>
             <main>
                 <Socials/>
             </main>
