c6acf09aad4f131dea797e08f60439fe050fdd6b
Danny Ochoa <danny@dannyochoa.net>
2024-11-13 14:17:42 -0500

    Add ESLint and Prettier

--- src/app/components/sections-header.tsx
+++ src/app/components/sections-header.tsx
@@ -1,13 +1,20 @@
-import styles from './sections-header.module.css';
-import Link from 'next/link';
+import styles from "./sections-header.module.css";
+import Link from "next/link";
 
 const SectionsHeader = ({ className }) => {
-    return (
-        <div className={className}>
-            <Link href="/" className={`${styles["header-entry"]} font-bold inline-block relative ml-0`}>Home</Link>
-            <Link href="/contact" className={`${styles["header-entry"]} font-bold inline-block relative mr-0`}>Contact</Link>
-         </div>
-      ); 
+  return (
+    <div className={className}>
+      <Link href="/" className={`${styles["header-entry"]} font-bold inline-block relative ml-0`}>
+        Home
+      </Link>
+      <Link
+        href="/contact"
+        className={`${styles["header-entry"]} font-bold inline-block relative mr-0`}
+      >
+        Contact
+      </Link>
+    </div>
+  );
 };
 
-export default SectionsHeader
+export default SectionsHeader;
