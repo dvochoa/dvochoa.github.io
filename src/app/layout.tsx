c4a290092b05d11f798a994a598c7022f49f4953
Danny Ochoa <danny@dannyochoa.net>
2024-12-09 22:38:24 -0500

    Trying to get mobile favicons to work some more :(

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,13 @@ import "@/styles/global.css";
 
 export const metadata = {
   description: "Danny Ochoa's personal website",
+  icons: {
+    icon: [
+      { url: "/favicon/icon.svg", sizes: "32x32", type: "image/svg+xml" },
+      { url: "/favicon/icon.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: [{ url: "/favicon/apple-icon.png", sizes: "180x180", type: "image/png" }],
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
